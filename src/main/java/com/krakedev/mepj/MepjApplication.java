5dcdb7ae8d95e5a833435091c3d312add42ad2d9
Junior27VQ <favq1999@outlook.com>
2026-05-16 19:30:23 -0500

    new project

---
+++ src/main/java/com/krakedev/mepj/MepjApplication.java
@@ -0,0 +1,13 @@
+package com.krakedev.mepj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MepjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MepjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/krakedev/mepj/MepjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krakedev.mepj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MepjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
