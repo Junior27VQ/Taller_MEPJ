6b158adcf0f53663e0f2341390bf61add36492cb
Junior27VQ <favq1999@outlook.com>
2026-05-16 20:57:46 -0500

    FAVQ: read file with multiple catch

---
+++ src/main/java/com/krakedev/mepj/Excepciones/LeerContacto.java
@@ -0,0 +1,39 @@
+package com.krakedev.mepj.Excepciones;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class LeerContacto {
+private static final Logger log =LogManager.getLogger(LeerContacto.class);
+	
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		String archivo="contactos.txt";
+	
+		FileReader lectorArchivo=null;
+		BufferedReader lector=null;
+		
+		try {
+			lectorArchivo = new FileReader(archivo);
+			lector =new BufferedReader(lectorArchivo);
+			log.info(lector.readLine());
+		}catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			log.error("Error al abrir el archivo");
+		}catch (IOException e) {
+			// TODO Auto-generated catch block
+			log.error("Error al leer el archivo");
+		}finally {
+			lector.close();
+		}
+		//solo se imprime el primer catch y el segundo me salta un nullpionterexception
+		//al invertir los catch me sale que el primer bloque catch ya maneja el error
+		//
+	}
+
+}
